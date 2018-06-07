c6311768770058f9972846aed86f1aa3ebae337d
Carlos Andres Chapid Inga <carloschapid@unicauca.edu.co>
2018-06-07 15:00:26 -0500

    Arreglo de llaves por parentesis

--- GUI_Inicio.rkt
+++ GUI_Inicio.rkt
@@ -4,35 +4,43 @@
 ;Definimos el frame principal, inicio
 (define application-frame
   (new frame%
-    [label "VACUNAS - RACKET"]
-    [width 400]
-    [height 300]
-    [alignment '(center top)]))
+    (label "VACUNAS - RACKET")
+    (width 400)
+    (height 300)
+    (alignment '(center top))))
 
 ;Agregamos un panel al frame
 (define panel1 (new vertical-panel%
-                    [alignment '(center center)]
-                    [parent application-frame]))
+                    (alignment '(center center))
+                    (parent application-frame)))
+                
 
-(define txtRegistroCivil (new text-field% [parent panel1](vert-margin 10)(horiz-margin 50)[min-height 10][min-width 150][label "Registro Civil"]))
+(define txtRegistroCivil (new text-field% 
+                          (parent panel1)
+                          (vert-margin 10)
+                          (horiz-margin 50)
+                          (min-height 10)
+                          (min-width 150)
+                          (label "Registro Civil")))
+                          
 (define msgRes (new message% [parent panel1][label ""][min-height 1][min-width 400]))
 
 (define btnRegistrarMenor(new button%
-                        [parent panel1]
-                        [min-width 150]
-                        [label "Registrar Menor"]))
+                          (parent panel1)
+                          (min-width 150)
+                          (label "Registrar Menor")))
 (define btnConsultarCitas(new button%
-                        [parent panel1]
-                        [min-width 150]
-                        [label "Consultar Citas"]))
+                          (parent panel1)
+                          (min-width 150)
+                          (label "Consultar Citas")))
 (define btnCitas(new button%
-                        [parent panel1]
-                        [min-width 150]
-                        [label "Registrar Vacuna"]))
+                        (parent panel1)
+                        (min-width 150)
+                        (label "Registrar Vacuna")))
 (define btnVerReportes(new button%
-                        [parent panel1]
-                        [min-width 150]
-                        [label "Ver Reportes"]))
+                        (parent panel1)
+                        (min-width 150)
+                        (label "Ver Reportes")))
   
 
 ; Mostramos el frameprincipal
--- views/GUI_RegistroMenor.rkt
+++ views/GUI_RegistroMenor.rkt
@@ -8,111 +8,111 @@
 
 (define registro-menor-frame
   (new frame%
-    [label "Registro menor - VACUNAS RACKET"]
-    [width 600]
-    [height 400]))
+    (label "Registro menor - VACUNAS RACKET")
+    (width 600)
+    (height 400)))
 
 ;Definicion de grupos
-(define grupoMenor(new group-box-panel%	 
-   	 	[label "Datos del menor"]
-                [vert-margin 5]
-                [horiz-margin 50]
-                [parent registro-menor-frame]
-                ))
-(define grupoContacto(new group-box-panel%	 
-   	 	[label "Datos del contacto"]
-                [vert-margin 5]
-                [horiz-margin 50]
-                [parent registro-menor-frame]
-                ))
+(define grupoMenor(new group-box-panel%   
+                   (label "Datos del menor")
+                   (vert-margin 5)
+                   (horiz-margin 50)
+                   (parent registro-menor-frame)))
+                
+(define grupoContacto(new group-box-panel%   
+                      (label "Datos del contacto")
+                      (vert-margin 5)
+                      (horiz-margin 50)
+                      (parent registro-menor-frame)))
+                
 ;Campos del menor
 (define txtRegistroCivil (new text-field%
-                              [parent grupoMenor]
+                              (parent grupoMenor)
                               (horiz-margin 50)
-                              [min-height 10]
-                              [min-width 150]
-                              [label "Registro Civil"]
-                              ))
+                              (min-height 10)
+                              (min-width 150)
+                              (label "Registro Civil")))
+                              
 (define txtNombresApellidos (new text-field%
-                              [parent grupoMenor]
+                              (parent grupoMenor)
                               (horiz-margin 50)
-                              [min-height 10]
-                              [min-width 150]
-                              [label "Nombres y Apellidos"]
-                              ))
+                              (min-height 10)
+                              (min-width 150)
+                              (label "Nombres y Apellidos")))
+                              
 (define txtFechaNacimiento (new text-field%
-                              [parent grupoMenor]
+                              (parent grupoMenor)
                               (horiz-margin 50)
-                              [min-height 10]
-                              [min-width 150]
-                              [label "Fecha Nacimiento"]
-                              ))
+                              (min-height 10)
+                              (min-width 150)
+                              (label "Fecha Nacimiento")))
+                              
 (define comboGenero (new combo-field%
-                         [choices '("M" "F")]
-                         [label "Género"]
-                         [init-value ""]	 
-                         [horiz-margin 50]
-                         [min-height 10][min-width 150]
-                         [parent grupoMenor]
-                         ))
+                         (choices '("M" "F"))
+                         (label "Género")
+                         (init-value "")   
+                         (horiz-margin 50)
+                         (min-height 10)(min-width 150)
+                         (parent grupoMenor)))
+                         
 
 ;Campos del Contato
 (define txtNombreContacto (new text-field%
-                               [parent grupoContacto]
+                               (parent grupoContacto)
                                (horiz-margin 50)
-                               [min-height 10]
-                               [min-width 150]
-                               [label "Nombre (Padre/Madre)"]))
+                               (min-height 10)
+                               (min-width 150)
+                               (label "Nombre (Padre/Madre)")))
 (define txtTelefonoContacto (new text-field%
-                               [parent grupoContacto]
+                               (parent grupoContacto)
                                (horiz-margin 50)
-                               [min-height 10]
-                               [min-width 150]
-                               [label "Telefono Contacto"]))
+                               (min-height 10)
+                               (min-width 150)
+                               (label "Telefono Contacto")))
 (define txtDireccionContacto (new text-field%
-                               [parent grupoContacto]
-                               [horiz-margin 50]
-                               [min-height 10]
-                               [min-width 150]
-                               [label "Dirección Contacto"]))
+                               (parent grupoContacto)
+                               (horiz-margin 50)
+                               (min-height 10)
+                               (min-width 150)
+                               (label "Dirección Contacto")))
 ;Mensaje
-(define msgRes (new message% [parent registro-menor-frame][label ""][min-height 1][min-width 400]))
+(define msgRes (new message% (parent registro-menor-frame)(label "")(min-height 1)(min-width 400)))
 
 (define panelBotones (new horizontal-panel%
-                    [alignment '(center top)]
-                    [parent registro-menor-frame]))
+                      (alignment '(center top))
+                      (parent registro-menor-frame)))
 
 ;TODO 1: AGREGAR MENSAJES DE GUARDADO
 
-(define btnGuardar(new button%
-                        [parent panelBotones]
-                        [enabled #t]
-                        [label "REGISTRAR"]
-                        [callback (lambda (button event)
+(define btnGuardar (new button%
+                        (parent panelBotones)
+                        (enabled #t)
+                        (label "REGISTRAR")
+                        (callback (lambda (button event)
                                     (send msgRes set-label (
-                                                           obtieneDatosMenor 
-                                                           (send txtRegistroCivil get-value)
+                                                            obtieneDatosMenor 
+                                                            (send txtRegistroCivil get-value)
                                                             (send txtNombresApellidos get-value)
                                                             (send txtFechaNacimiento get-value)
                                                             (send comboGenero get-value)
                                                             (send txtNombreContacto get-value)
                                                             (send txtTelefonoContacto get-value)
-                                                            (send txtDireccionContacto get-value))))]
-                        ))
+                                                            (send txtDireccionContacto get-value)))))))
+                        
 
 (define btnLimpiarCampos(new button%
-                        [parent panelBotones]
-                        [enabled #t]
-                        [label "LIMPIAR CAMPOS"]
-                        [callback (lambda (button event)
-                                    (limpiarCampos txtRegistroCivil
-                        txtNombresApellidos
-                        txtFechaNacimiento
-                        comboGenero
-                        txtNombreContacto
-                        txtTelefonoContacto
-                        txtDireccionContacto))]
-                      ))  
+                         (parent panelBotones)
+                         (enabled #t)
+                         (label "LIMPIAR CAMPOS")
+                         (callback (lambda (button event)
+                                     (limpiarCampos txtRegistroCivil
+                                      txtNombresApellidos
+                                      txtFechaNacimiento
+                                      comboGenero
+                                      txtNombreContacto
+                                      txtTelefonoContacto
+                                      txtDireccionContacto)))))
+                        
 
 (define (limpiarCampos
          txtRegistroCivil
@@ -122,14 +122,14 @@
          txtNombreContacto
          txtTelefonoContacto
          txtDireccionContacto)
-         (cond
-            [(not (eqv? txtRegistroCivil "")) (send txtRegistroCivil set-value "")]
-            [(not (eqv? txtNombresApellidos "")) (send txtNombresApellidos set-value "")]
-            [(not (eqv? txtFechaNacimiento "")) (send txtFechaNacimiento set-value "")]
-            [(not (eqv? txtNombreContacto "")) (send txtNombreContacto set-value "")]
-            [(not (eqv? txtTelefonoContacto "")) (send txtTelefonoContacto set-value "")]
-            [(not (eqv? txtDireccionContacto "")) (send txtDireccionContacto set-value "")]
-            [(not (eqv? comboGenero "")) (send comboGenero set-value "")]
-            ))
+        (cond
+           ((not (eqv? txtRegistroCivil "")) (send txtRegistroCivil set-value ""))
+           ((not (eqv? txtNombresApellidos "")) (send txtNombresApellidos set-value ""))
+           ((not (eqv? txtFechaNacimiento "")) (send txtFechaNacimiento set-value ""))
+           ((not (eqv? txtNombreContacto "")) (send txtNombreContacto set-value ""))
+           ((not (eqv? txtTelefonoContacto "")) (send txtTelefonoContacto set-value ""))
+           ((not (eqv? txtDireccionContacto "")) (send txtDireccionContacto set-value ""))
+           ((not (eqv? comboGenero "")) (send comboGenero set-value ""))))
+            
 
 (send registro-menor-frame show #t)
