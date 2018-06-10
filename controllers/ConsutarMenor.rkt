b4a687edbe9c87c1106d839163ff9382cb57b65e
renejal <beimarjal@unicauca.edu.co>
2018-06-10 12:44:01 -0500

    se a actulizado el formato de vacunacion

--- controllers/ConsutarMenor.rkt
+++ controllers/ConsutarMenor.rkt
@@ -8,20 +8,40 @@
 (provide listarTodasFechas)
 (provide listarTodasEtapas)
 (provide listarDatosMenor)
-
+(provide listarTodasFechasReales)
+(provide listarTodasEstados)
+(provide listarTodasVacunas)
+(provide listarDatosMenor)
 
 (define (listarTodasFechas regCivil) (query-list conn listarTodasFechasQuery regCivil))
 (define (listarTodasEtapas regCivil)(query-list conn listarTodasEtapasQuery regCivil))
+(define (listarTodasFechasReales regCivil)(query-list conn listarTodasFechasRealesQuery regCivil))
+(define (listarTodasEstados regCivil)(query-list conn listarTodasEstadosQuery regCivil))
+(define (listarTodasVacunas regCivil)(query-list conn listarTodasVacunasQuery regCivil))
 (define (listarDatosMenor regCivil)(vector->list (query-row conn listarMenorQuery regCivil)))
 ; (define getComunasSQL (query-list conn "SELECT com_nombre FROM COMUNAS"))
 ; (define getFrutasSQL (query-list conn "SELECT fru_nombre FROM FRUTAS"))
 ; (define getNutrientesSQL (query-list conn "SELECT nut_nombre FROM NUTRIENTES"))
 
-(define listarTodasFechasQuery (prepare conn "SELECT DATE_FORMAT(pacienteetapa.FECHA, '%Y-%m-%d') from pacienteetapa inner join etapa ON pacienteetapa.IDETAPA = etapa.IDETAPA WHERE pacienteetapa.IDREGISTROCIVIL = ? ORDER BY etapa.IDETAPA"))
+(define listarTodasFechasQuery (prepare conn "SELECT DATE_FORMAT(pacienteetapa.FECHA,'%d %m %Y') FROM pacienteetapa INNER JOIN paciente ON pacienteetapa.IDREGISTROCIVIL = paciente.IDREGISTROCIVIL INNER JOIN etapa ON etapa.IDETAPA = pacienteetapa.IDETAPA INNER JOIN etapavacuna ON etapa.IDETAPA = etapavacuna.IDETAPA INNER JOIN vacuna ON vacuna.IDVACUNA = etapa.IDETAPA
+WHERE paciente.IDREGISTROCIVIL = ?
+ORDER BY FECHA"))
+(define listarTodasEtapasQuery (prepare conn "SELECT etapa.nombre FROM pacienteetapa INNER JOIN paciente ON pacienteetapa.IDREGISTROCIVIL = paciente.IDREGISTROCIVIL INNER JOIN etapa ON etapa.IDETAPA = pacienteetapa.IDETAPA INNER JOIN etapavacuna ON etapa.IDETAPA = etapavacuna.IDETAPA INNER JOIN vacuna ON vacuna.IDVACUNA = etapa.IDETAPA
+WHERE paciente.IDREGISTROCIVIL = ?
+ORDER BY FECHA"))
+(define listarTodasFechasRealesQuery (prepare conn "SELECT DATE_FORMAT(pacienteetapa.FECHAVACUNACION, '%d %m %Y') FROM pacienteetapa INNER JOIN paciente ON pacienteetapa.IDREGISTROCIVIL = paciente.IDREGISTROCIVIL INNER JOIN etapa ON etapa.IDETAPA = pacienteetapa.IDETAPA INNER JOIN etapavacuna ON etapa.IDETAPA = etapavacuna.IDETAPA INNER JOIN vacuna ON vacuna.IDVACUNA = etapa.IDETAPA
+WHERE paciente.IDREGISTROCIVIL = ?
+ORDER BY FECHA"))
+(define listarTodasEstadosQuery (prepare conn "SELECT pacienteetapa.ESTADO FROM pacienteetapa INNER JOIN paciente ON pacienteetapa.IDREGISTROCIVIL = paciente.IDREGISTROCIVIL INNER JOIN etapa ON etapa.IDETAPA = pacienteetapa.IDETAPA INNER JOIN etapavacuna ON etapa.IDETAPA = etapavacuna.IDETAPA INNER JOIN vacuna ON vacuna.IDVACUNA = etapa.IDETAPA
+WHERE paciente.IDREGISTROCIVIL = ?
+ORDER BY FECHA" ))
+(define listarTodasVacunasQuery (prepare conn "SELECT vacuna.NOMBRE FROM pacienteetapa INNER JOIN paciente ON pacienteetapa.IDREGISTROCIVIL = paciente.IDREGISTROCIVIL INNER JOIN etapa ON etapa.IDETAPA = pacienteetapa.IDETAPA INNER JOIN etapavacuna ON etapa.IDETAPA = etapavacuna.IDETAPA INNER JOIN vacuna ON vacuna.IDVACUNA = etapa.IDETAPA
+WHERE paciente.IDREGISTROCIVIL = ?
+ORDER BY FECHA"))
 
 ; FORMAT(VALUE,'yyyy-MM-dd')
 
-(define listarTodasEtapasQuery (prepare conn "SELECT etapa.NOMBRE from pacienteetapa inner join etapa ON pacienteetapa.IDETAPA = etapa.IDETAPA WHERE pacienteetapa.IDREGISTROCIVIL = ?  ORDER BY etapa.IDETAPA"))
+;(define listarTodasEtapasQuery (prepare conn "SELECT etapa.NOMBRE from pacienteetapa inner join etapa ON pacienteetapa.IDETAPA = etapa.IDETAPA WHERE pacienteetapa.IDREGISTROCIVIL = ?  ORDER BY etapa.IDETAPA"))
 (define listarMenorQuery (prepare conn "SELECT IDREGISTROCIVIL, NOMBRESAPELLIDOS, DATE_FORMAT(FECHANACIMIENTO, '%Y-%m-%d'), GENERO, NOMBRECONTACTO, TELEFONOCONTACTO, DIRECCION, GENERO FROM bdvacunacion.paciente WHERE IDREGISTROCIVIL = ?  LIMIT 1"))
 
 (listarDatosMenor 123)
